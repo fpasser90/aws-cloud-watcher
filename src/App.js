2dd3c614afd3cc47dfaecf5c1dbb9605c9c64267
Farrukh Ehsan <fpasser90@passer.digital>
2021-09-06 17:02:09 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React, {useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
+// import * as AWS from 'aws-sdk'
+import * as AWS from "@aws-sdk/client-cloudwatch-logs";
+const client = new AWS.CloudWatchLogs({ region: "us-east-2" });
 
 function App() {
+  useEffect(() => {
+    // let params = {
+    //     MetricData: [ 
+    //     {
+    //       MetricName: `DEMO_COUNT`, 
+    //       Timestamp: new Date,
+    //       Unit: 'Count',
+    //       Value: 1.0
+    //     }
+    //     ],
+    //     Namespace: 'STARTUP_KIT/API'  
+    // }
+    // client
+    // .associateKmsKey(params)
+    // .then((data) => {
+    //   console.log('cloudWatch data', data)
+    //   // process data.
+    // })
+    // .catch((error) => {
+    //   console.log('cloudWatch error', error)
+    //   // error handling.
+    // });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
